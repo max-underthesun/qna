b4091858ad5a98d54463c6754d33e543701e94f3
max_underthesun <max.underthesun@gmail.com>
2015-11-13 13:44:40 +0400

    spec 'GET #index' REFACTOR

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,17 +2,14 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
+    let(:questions) { create_list(:question, 2) }
+    before { get :index }
+
     it 'puts all the questions to the array' do
-      question_1 = create(:question)
-      question_2 = create(:question)
-
-      get :index
-
-      expect(assigns(:questions)).to match_array([question_1, question_2])
+      expect(assigns(:questions)).to match_array(questions)
     end
 
     it 'render index view' do
-      get :index
       expect(response).to render_template :index
     end
   end
