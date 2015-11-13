1215ceef04a5ee28f3bb859585445e82794e5bd9
max_underthesun <max.underthesun@gmail.com>
2015-11-13 19:59:09 +0400

    Answer spec 'POST #create' - successful saving part RED-GREEN, plus add question assigning for 'new' (spec and controller)

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,22 @@
 class AnswersController < ApplicationController
+  before_action :set_question, only: [:new, :create]
+
   def new
     @answer = Answer.new
+  end
+
+  def create
+    @answer = @question.answers.create(answer_params)
+    redirect_to @question
+  end
+
+  private
+
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body, :question_id)
   end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
+
   describe 'GET #new' do
-    let(:question) { create(:question) }
     before { get :new, question_id: question }
+
+    it 'puts the question to the variable @question' do
+      expect(assigns(:question)).to eq question
+    end
 
     it 'puts a new Answer in to the @answer' do
       expect(assigns(:answer)).to be_a_new(Answer)
@@ -14,4 +19,19 @@ RSpec.describe AnswersController, type: :controller do
     end
   end
 
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new answer to the database' do
+        expect { post :create, question_id: question, answer: attributes_for(:answer, question_id: question) }.to change(Answer, :count).by(1)
+      end
+
+      it 'redirect to answer question show view' do
+        post :create, question_id: question, answer: attributes_for(:answer, question_id: question)
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context 'with invalid attributes' do
+    end
+  end
 end
