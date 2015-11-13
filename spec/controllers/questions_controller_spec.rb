0e358ad17b50012ec36e69dd57c1065f2c4e7453
max_underthesun <max.underthesun@gmail.com>
2015-11-13 17:15:48 +0400

    spec 'POST #create' RED-GREEN

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -38,4 +38,30 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template :new
     end
   end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new question to the database' do
+        expect { post :create, question: attributes_for(:question) }
+          .to change(Question, :count).by(1)
+      end
+
+      it 'redirect to show' do
+        post :create, question: attributes_for(:question)
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the question in the database' do
+        expect { post :create, question: attributes_for(:invalid_question) }
+          .to_not change(Question, :count)
+      end
+
+      it 'render new view' do
+        post :create, question: attributes_for(:invalid_question)
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
