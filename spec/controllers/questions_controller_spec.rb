6901046294999bab02239256556aa26287c9c049
max_underthesun <max.underthesun@gmail.com>
2015-11-13 10:39:17 +0400

    fill in 'GET #index' spec for QuestionController (first edition): RED

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  describe 'GET #index' do
+    it 'puts all the questions to the array' do
+      question_1 = create(:question)
+      question_2 = create(:question)
 
+      get :index
+
+      expect(assign(:questions)).to match_array([question_1, question_2])
+    end
+
+    it 'render index view' do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
 end
