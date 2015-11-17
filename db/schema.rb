b17c866fd1acd50a21c101bccdc71c9b67a49fe4
max_underthesun <max.underthesun@gmail.com>
2015-11-17 09:38:10 +0400

    create acceptance test for sign_in, model test for User, generate User model and add initializer for test failing in case of key absence in locale

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151111143809) do
+ActiveRecord::Schema.define(version: 20151117053303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,11 @@ ActiveRecord::Schema.define(version: 20151111143809) do
   create_table "questions", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/acceptance/sign_in_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+feature 'Signing in', %q{
+  In order to be able to ask questions
+  As a user
+  I want to be able to sing in
+} do
+  # given(:user) {create(:user)}
+
+  scenario 'Registered user try to sign in' do
+    User.create(email: 'user@test.com', password: '12345678')
+
+    visit new_user_session_path
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Sing in'
+
+    expect(page).to have_content 'Signed in successfully.'
+    expect(current_path).to_eq root_path
+  end
+
+  scenario 'Unregistered user try to sign in' do
+    visit new_user_session_path
+    fill_in 'Email', with: 'wrong@test.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Sing in'
+
+    expect(page).to have_content 'Invalid email or password.'
+    expect(current_path).to_eq new_user_session_path
+  end
+end
