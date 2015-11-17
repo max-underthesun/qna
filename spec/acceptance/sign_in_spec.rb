dec097b2a1175e3f4a8896adc2985d50b57662b3
max_underthesun <max.underthesun@gmail.com>
2015-11-17 11:07:17 +0400

    small edits in sign_in test for successful finish with tested feature

--- spec/acceptance/sign_in_spec.rb
+++ spec/acceptance/sign_in_spec.rb
@@ -13,19 +13,19 @@ feature 'Signing in', %q{
     visit new_user_session_path
     fill_in 'Email', with: 'user@test.com'
     fill_in 'Password', with: '12345678'
-    click_on 'Sing in'
+    click_on 'Log in'
 
     expect(page).to have_content 'Signed in successfully.'
-    expect(current_path).to_eq root_path
+    expect(current_path).to eq root_path
   end
 
   scenario 'Unregistered user try to sign in' do
     visit new_user_session_path
     fill_in 'Email', with: 'wrong@test.com'
     fill_in 'Password', with: '12345678'
-    click_on 'Sing in'
+    click_on 'Log in'
 
     expect(page).to have_content 'Invalid email or password.'
-    expect(current_path).to_eq new_user_session_path
+    expect(current_path).to eq new_user_session_path
   end
 end
