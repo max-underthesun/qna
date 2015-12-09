adad169e0698efde48659d43e484e71591915922
max_underthesun <max.underthesun@gmail.com>
2015-12-09 17:36:17 +0400

    make 'best_answer_spec'

---
+++ spec/acceptance/answers/question_author_can_choose_best_answer_spec.rb
@@ -0,0 +1,61 @@
+require_relative '../acceptance_helper'
+
+feature 'BEST ANSWER', %q(
+  only signed in author of question should have abiliti to choose the best answer
+  for his question, best answer marked and positioned first in the list of answers
+) do
+  given(:user) { create(:user) }
+  given(:other_user) { create(:user) }
+  given!(:question) { create(:question, user: user) }
+  given!(:answers) { create_list(:answer, 5, question: question) }
+
+  # given!(:answer) { create(:answer, question: question, user: user) }
+  # given!(:other_answer) { create(:answer, question: question, user: other_user) }
+  # given(:updated_answer) { build(:answer) }
+  # given(:invalid_answer) { build(:invalid_answer) }
+
+  scenario "- unauthenticated user do not see 'Best answer' button" do
+    visit question_path(question)
+
+    within '.answers' do
+      expect(page).to_not have_button(I18n.t('buttons.best_answer'))
+    end
+  end
+
+  scenario "- authenticated but not the question author user do not see 'Best answer' button" do
+    sign_in(other_user)
+    visit question_path(question)
+
+    within '.answers' do
+      expect(page).to_not have_button(I18n.t('buttons.best_answer'))
+    end
+  end
+
+  describe "- question author can choose the best answer" do
+    before do
+      sign_in(user)
+      visit question_path(question)
+    end
+
+    scenario "-- author see the 'Best answer' buttons for each answer" do
+      answers.each do |answer|
+        within ".answer#answer_#{answer.id}" do
+          expect(page).to have_button(I18n.t('buttons.best_answer'))
+        end
+      end
+    end
+
+    scenario "-- author can choose any answer as best answer" do
+      answers.each do |answer|
+        within ".answer#answer_#{answer.id}" do
+          click_on I18n.t('buttons.best_answer')
+        end
+
+        expect(page).to have_css('.best_answer')
+        within ".best_answer" do
+          expect(page).to have answer.body
+        end
+      end
+    end
+  end
+end
