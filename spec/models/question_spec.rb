0c662e81008fe66b47f19d275a85c634d69560b3
max_underthesun <max.underthesun@gmail.com>
2015-12-14 14:02:22 +0400

    add gem 'carrierwawe' and Attachment model (with specs)

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe Question, type: :model do
   it { should validate_presence_of :title }
   it { should validate_length_of(:title).is_at_most(150) }
   it { should validate_presence_of :body }
+
   it { should have_many :answers }
+  it { should have_many :attachments }
 
   it { should validate_presence_of :user_id }
   it { should belong_to :user }
