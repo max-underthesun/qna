94d32a517f0ab41eb91fb58e62c8cf46f4009d19
max_underthesun <max.underthesun@gmail.com>
2016-01-02 12:46:30 +0400

    finish with scenario 'anauthorized user see the question rating' in VOTE FOR THE QUESTION spec

--- spec/acceptance/votes/vote_for_question_spec.rb
+++ spec/acceptance/votes/vote_for_question_spec.rb
@@ -7,6 +7,7 @@ feature 'VOTE FOR THE QUESTION', %q(
   given(:user) { create(:user) }
   given(:question_author) { create(:user) }
   given!(:question) { create(:question, user: question_author) }
+  given!(:votes) { create_list(:vote, 5, votable: question) }
 
   scenario "- anauthorized user can see the rating of the question in the question show view" do
     visit question_path(question)
@@ -17,10 +18,10 @@ feature 'VOTE FOR THE QUESTION', %q(
     end
   end
 
-  describe "- author unable to vote for his question" do
-    scenario "-- author see the rating"
-    scenario "-- author don't see the buttons"
-  end
+  # describe "- author unable to vote for his question" do
+  #   scenario "-- author see the rating"
+  #   scenario "-- author don't see the buttons"
+  # end
 
   describe "- non-author able to vote for the question" do
     before do
@@ -29,10 +30,10 @@ feature 'VOTE FOR THE QUESTION', %q(
     end
 
     scenario "-- see the '+' and '-' buttons for vote"
-    scenario "-- can vote '+'"
-    scenario "-- can vote '-'"
-    scenario "-- unable to vote twice for the question (don't see buttons)"
-    scenario "-- can cancel his vote (have a button and it works properly)"
-    scenario "-- can cancel existent vote and make a new vote"
+    # scenario "-- can vote '+'"
+    # scenario "-- can vote '-'"
+    # scenario "-- unable to vote twice for the question (don't see buttons)"
+    # scenario "-- can cancel his vote (have a button and it works properly)"
+    # scenario "-- can cancel existent vote and make a new vote"
   end
 end
--- spec/factories/votes.rb
+++ spec/factories/votes.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :vote do
-    association :votable, factory: :question
+    # association :votable, factory: :question
     value 1
-    # votable { |a| a.association(:question) }
+    votable { |a| a.association(:question) }
     user
   end
 end
