d9c8b0db1c9a9391f5eb7c7153d21f1c2ba6b4f2
max_underthesun <max.underthesun@gmail.com>
2016-01-09 15:39:41 +0400

    add tests for User#author_of and User#can_vote?

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,4 +15,32 @@ RSpec.describe User do
   it { should have_many :questions }
   it { should have_many :answers }
   it { should have_many :votes }
+
+  let(:answer_author) { create(:user) }
+  let(:answer) { create(:answer, user: answer_author) }
+
+  describe "author_of?(object)" do
+    it " - should return 'true' if user is the author of the object" do
+      expect(answer_author.author_of?(answer)).to eq true
+    end
+
+    it " - should return 'false' if user is not the author of the object" do
+      expect(user.author_of?(answer)).to eq false
+    end
+  end
+
+  describe "can_vote?(object)" do
+    it " - return 'true' if user is not the author of the object and did not voted for it yet" do
+      expect(user.can_vote?(answer)).to eq true
+    end
+
+    it " - return 'false' if user is not the author of the object and already voted for it" do
+      create(:vote, votable: answer, user: user)
+      expect(user.can_vote?(answer)).to eq false
+    end
+
+    it " - return 'false' if user is author of object" do
+      expect(answer_author.can_vote?(answer)).to eq false
+    end
+  end
 end
