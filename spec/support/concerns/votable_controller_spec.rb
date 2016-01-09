b4b09c4768d9c3b96797505f15722b12020c4c84
max_underthesun <max.underthesun@gmail.com>
2016-01-09 10:41:20 +0400

    clear votable_controller_spec

--- spec/support/concerns/votable_controller_spec.rb
+++ spec/support/concerns/votable_controller_spec.rb
@@ -4,9 +4,6 @@ RSpec.shared_examples "votable_controller" do
   describe 'PATCH #vote_up' do
     describe 'for not signed in user: ' do
       it '- should not add new vote to the resource votes' do
-        # original_object = object
-        # patch :vote_up, id: object
-        # expect(object.vote.reload).to eq original_answer
         expect { patch :vote_up, id: resource, format: :json }
           .to_not change(resource.votes, :count)
       end
@@ -47,26 +44,6 @@ RSpec.shared_examples "votable_controller" do
         patch :vote_up, id: resource, format: :json
         expect(response).to have_http_status(:unprocessable_entity)
       end
-
-      # context 'with other answer choosen best before' do
-      #   let!(:previous_best_answer) {
-      #     create(:answer, question: question, user: answer_author, best: best_answer.best)
-      #   }
-
-      #   it '- switch previous best answer best status to false' do
-      #     patch :best, id: answer, answer: { best: best_answer.best }, format: :js
-      #     expect(previous_best_answer.reload.best).to_not eq best_answer.best
-      #   end
-
-      #   it '- switch answer best status to true' do
-      #     patch :best, id: answer, answer: { best: best_answer.best }, format: :js
-      #     expect(answer.reload.best).to eq best_answer.best
-      #   end
-
-      #   it '- render answer best template' do
-      #     patch :best, id: answer, answer: { best: best_answer.best }, format: :js
-      #     expect(response).to render_template :best
-      #   end
     end
   end
 end
