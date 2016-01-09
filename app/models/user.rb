3a6d067cd8948217498515e6ed2c7bfdae1fee93
max_underthesun <max.underthesun@gmail.com>
2016-01-09 14:49:40 +0400

    refactoring: move 'user can vote?' from view to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,17 @@ class User < ActiveRecord::Base
     object.user_id == id
   end
 
+  def can_vote?(object)
+    not_author_of?(object) && not_voted_yet_for?(object)
+  end
+
+  private
+
   def not_author_of?(object)
     !author_of?(object)
+  end
+
+  def not_voted_yet_for?(object)
+    (votes.find_by(votable_id: object.id, votable_type: object.class.to_s)).nil?
   end
 end
