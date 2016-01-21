450810eafcf46483a2d6f8f9c9f026b9d9cf9066
max_underthesun <max.underthesun@gmail.com>
2016-01-21 09:53:26 +0400

    add model Comment (+ spec and db migration)

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  subject { build(:comment, :question_comment) }
+
+  it { should belong_to :user }
+  it { should validate_presence_of :user_id }
+
+  it { should belong_to :commentable }
+  it { should validate_presence_of :commentable_id }
+  it { should validate_presence_of :commentable_type }
+
+  it { should validate_presence_of :body }
+end
