46982585832fe7c842b332f883b1d857feb04b87
max_underthesun <max.underthesun@gmail.com>
2016-01-22 10:30:44 +0400

    add order_by_creation_asc scope to Comment and use it in _comments partial

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,6 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
 
   validates :body, :user_id, :commentable_id, :commentable_type, presence: true
+
+  scope :order_by_creation_asc, -> { order(created_at: :asc) }
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -11,4 +11,19 @@ RSpec.describe Comment, type: :model do
   it { should validate_presence_of :commentable_type }
 
   it { should validate_presence_of :body }
+
+  describe "order_by_creation_asc should: " do
+    let(:question) { create(:question) }
+    let!(:comments) { create_list(:comment, 5, commentable: question) }
+
+    it "- order comments by creation" do
+      ordered_comments = question.comments.order_by_creation_asc.to_a
+
+      created = ordered_comments.first.created_at
+      ordered_comments.each do |comment|
+        expect(comment.created_at).to be >= created
+        created = comment.created_at
+      end
+    end
+  end
 end
