7327a4dd675fd92f1454efca663f8c681e72f366
max_underthesun <max.underthesun@gmail.com>
2016-01-28 10:39:11 +0400

    add configurations for facebook to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
 
   def author_of?(object)
     object.user_id == id
