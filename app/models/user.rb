73050e6465e68dc16c90155d472234a797e2cac2
max_underthesun <max.underthesun@gmail.com>
2016-01-28 12:53:31 +0400

    add User.find_for_oauth method working in case 'user already has authorization'

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,18 @@ class User < ActiveRecord::Base
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
   has_many :votes, dependent: :destroy
+  has_many :authorizations, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:facebook]
+
+  def self.find_for_oauth(auth)
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    authorization.user if authorization
+  end
 
   def author_of?(object)
     object.user_id == id
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe User do
   let(:answer_author) { create(:user) }
   let(:answer) { create(:answer, user: answer_author) }
 
-  describe "author_of?(object)" do
+  describe "#author_of?(object)" do
     it " - should return 'true' if user is the author of the object" do
       expect(answer_author.author_of?(answer)).to eq true
     end
@@ -29,7 +29,7 @@ RSpec.describe User do
     end
   end
 
-  describe "can_vote?(object)" do
+  describe "#can_vote?(object)" do
     it " - return 'true' if user is not the author of the object and did not voted for it yet" do
       expect(user.can_vote?(answer)).to eq true
     end
@@ -41,6 +41,18 @@ RSpec.describe User do
 
     it " - return 'false' if user is author of object" do
       expect(answer_author.can_vote?(answer)).to eq false
+    end
+  end
+
+  describe ".find_for_oauth" do
+    let!(:user) { create(:user) }
+    let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456') }
+
+    context "user already has authorization" do
+      it "returns the user" do
+        user.authorizations.create(provider: 'facebook', uid: '123456')
+        expect(User.find_for_oauth(auth)).to eq user
+      end
     end
   end
 end
