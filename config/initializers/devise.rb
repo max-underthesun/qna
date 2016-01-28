ebed10a33b2b1efba0c32c153b06e76ca07c2d36
max_underthesun <max.underthesun@gmail.com>
2016-01-28 11:27:22 +0400

    fix error in devise.rb and add omniauth_callbacks_controller

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -238,7 +238,7 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook,
                   Rails.application.secrets.facebook_app_id,
-                  Rails.application.secrets.facebook_app_id,
+                  Rails.application.secrets.facebook_app_secret,
                   info_fields: 'id,email'
 
   # ==> Warden configuration
