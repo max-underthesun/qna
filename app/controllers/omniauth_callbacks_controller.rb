87f4a0f29845b41590cd7fed6c35f70b5980ac2f
max_underthesun <max.underthesun@gmail.com>
2016-01-29 11:18:23 +0400

    move user authorization in OmniauthCallbacksController from #facebook action to a #authorize_user private method

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,9 +1,16 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  before_action :authorize_user
+
   def facebook
+  end
+
+  private
+
+  def authorize_user
     @user = User.find_for_oauth(request.env['omniauth.auth'])
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-    end
+    return unless @user.persisted?
+    sign_in_and_redirect @user, event: :authentication
+    return unless is_navigational_format?
+    set_flash_message(:notice, :success, kind: "#{action_name}".capitalize)
   end
 end
