b00fa7a8ab156c95b6fe4c8f1d4869506d33b41c
max_underthesun <max.underthesun@gmail.com>
2016-02-02 17:36:10 +0400

    clear User from code moved to OauthAuthenticator

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,21 +10,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:facebook, :twitter]
 
-  # def self.find_for_oauth(auth)
-  #   @email = auth.info[:email] if auth.info.try(:email)
-
-  #   if user_exists_and_already_has_authorization?(auth)
-  #     return @user
-  #   elsif user_exists_but_has_no_authorization?
-  #     create_authorization(auth)
-  #   else
-  #     return unless @email
-  #     create_a_new_user
-  #     create_authorization(auth)
-  #   end
-  #   @user
-  # end
-
   def author_of?(object)
     object.user_id == id
   end
@@ -38,24 +23,6 @@ class User < ActiveRecord::Base
   end
 
   private
-
-  # def self.user_exists_and_already_has_authorization?(auth)
-  #   @authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
-  #   @user = @authorization.user if @authorization
-  # end
-
-  # def self.user_exists_but_has_no_authorization?
-  #   @user = User.where(email: @email).first
-  # end
-
-  # def self.create_a_new_user
-  #   password = Devise.friendly_token[0, 20]
-  #   @user = User.create!(email: @email, password: password, password_confirmation: password)
-  # end
-
-  # def self.create_authorization(auth)
-  #   @user.authorizations.create(provider: auth.provider, uid: auth.uid)
-  # end
 
   def not_author_of?(object)
     !author_of?(object)
