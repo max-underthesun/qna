5f1632c828ad54c442f05660883e2986c5c7886b
max_underthesun <max.underthesun@gmail.com>
2016-02-15 16:49:33 +0400

    remove default scope from Comment and Attachment and use custom association builder instead of has_many in serializers

--- app/serializers/answer_serializer.rb
+++ app/serializers/answer_serializer.rb
@@ -1,5 +1,13 @@
 class AnswerSerializer < ActiveModel::Serializer
-  attributes :id, :body, :updated_at, :created_at, :user_id
-  has_many :comments
-  has_many :attachments
+  attributes :id, :body, :updated_at, :created_at, :user_id, :attachments, :comments
+  # has_many :comments
+  # has_many :attachments
+
+  def attachments
+    object.attachments.order_by_creation_asc.map {|att| AttachmentSerializer.new(att, root: false)}
+  end
+
+  def comments
+    object.comments.order_by_creation_asc.map {|att| CommentSerializer.new(att, root: false)}
+  end
 end
--- app/serializers/question_show_serializer.rb
+++ app/serializers/question_show_serializer.rb
@@ -1,5 +1,13 @@
 class QuestionShowSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :created_at, :updated_at
-  has_many :attachments
-  has_many :comments
+  attributes :id, :title, :body, :created_at, :updated_at, :comments, :attachments
+  # has_many :attachments
+  # has_many :comments
+
+  def attachments
+    object.attachments.order_by_creation_asc.map {|att| AttachmentSerializer.new(att, root: false)}
+  end
+
+  def comments
+    object.comments.order_by_creation_asc.map {|att| CommentSerializer.new(att, root: false)}
+  end
 end
