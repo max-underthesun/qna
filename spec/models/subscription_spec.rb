59f41298ed5cb3427097e84f56d78ddaa972a03e
max_underthesun <max.underthesun@gmail.com>
2016-02-24 18:47:13 +0400

    add uniqueness of 'user_id' with scope of 'question_id' validation to Subscription

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Subscription, type: :model do
 
   it { should validate_presence_of :question_id }
   it { should belong_to :question }
+
+  it do
+    create :subscription
+    is_expected.to validate_uniqueness_of(:user_id).scoped_to(:question_id)
+  end
 end
