9a4b5d94941338768d96d502af165527e33bfe34
max_underthesun <max.underthesun@gmail.com>
2016-03-03 16:27:56 +0400

    move part of the logic from SearchesController to Search

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,9 +1,37 @@
-class Search < ActiveRecord::Base
-  RESOURCES = [
-    %w(Everywhere ThinkingSphinx),
-    %w(Questions Question),
-    %w(Answers Answer),
-    %w(Comments Comment),
-    %w(Users User)
-  ]
+class Search
+  include ActiveModel::Validations
+  # extend ActiveModel::Callbacks
+
+  # RESOURCES = [
+  #   %w(Everywhere ThinkingSphinx),
+  #   %w(Questions Question),
+  #   %w(Answers Answer),
+  #   %w(Comments Comment),
+  #   %w(Users User)
+  # ]
+
+  # RESOURCES = %w(Everywhere Questions Answers Comments Users)
+
+  RESOURCES = {
+    'Everywhere' => ThinkingSphinx,
+    'Questions' => Question,
+    'Answers' => Answer,
+    'Comments' => Comment,
+    'Users' => User
+  }
+
+  attr_accessor :query, :scope #, :result
+
+  validates :query, :scope, presence: true
+  validates :scope, inclusion: RESOURCES.values
+
+  def initialize(params)
+    @query = params[:query]
+    @scope = RESOURCES[params[:scope]]
+    # valid?
+  end
+
+  def search_with(params)
+    @scope.search(Riddle.escape(@query), params)
+  end
 end
